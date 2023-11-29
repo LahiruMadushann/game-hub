f7ac247fb92c73764162c22ed30330f14920061f
LahiruMadushann <lahirumadushandl@gmail.com>
2023-11-29 20:34:51 +0530

    Implement Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`,
   }}>
-    <GridItem area='nav'>
+    <GridItem area='nav' >
       <NavBar />
     </GridItem>
     <Show above='lg'>
